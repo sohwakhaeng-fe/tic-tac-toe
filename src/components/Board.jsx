983d962730100a6a3a4b833921516c71bae01e2f
hyeon9782 <hyeon9782@gmail.com>
2023-04-21 15:50:17 +0900

    feat: 추가 기능 구현
    1. isNext state 삭제
    2. 현재 이동은 버튼 메세지 변경
    3. 버튼을 오름차순 또는 내림차순으로 정렬할 수 있는 토글 버튼 추기
    4. 이동 기록 목록에서 각 이동의 위치를 행 열 형식으로 표시

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -4,9 +4,12 @@ import Square from "./Square"
 const Board = () => {
     const [history, setHistory] = useState([Array.from(Array(3), () => Array.from(Array(3), () => null))]); // [squares],[],[],[],[],...,[]]
     const [currentStep, setCurrentStep] = useState(0);
-    const [isNext, setIsNext] = useState(false);
+    const [positions, setPositions] = useState([]);
+    const [isDesc, setIsDesc] = useState(false);
+    const isNext = currentStep % 2 === 0;
     const [isWin, setIsWin] = useState("");
     const handleClick = (clickedRow, clickedCol) => {
+        if(history.length === 10) return;
         
         setCurrentStep(index => ++index);
         
@@ -14,6 +17,17 @@ const Board = () => {
 
         if (currentSquares[clickedRow][clickedCol] !== null || isWin) return 
     
+
+        setPositions(prev => {
+            const newPositions = [...prev];
+            const newPosition =  {row: clickedRow, col: clickedCol};
+
+            if (!isDesc) newPositions.push(newPosition);
+            else newPositions.unshift(newPosition);
+           
+           return newPositions;
+        })
+
         const newSquares = currentSquares.map((rows, row) => {
             if(clickedRow !== row) return [...rows]
 
@@ -24,31 +38,51 @@ const Board = () => {
 
         const newHistory = [...history.slice(0, currentStep + 1), newSquares]
 
-        setIsNext(!isNext);
         setHistory(newHistory);
         judgeWinner(newSquares)
+ 
     }
 
     const move = (index) => {
-        setCurrentStep(index);
-        setIsNext(!isNext);
+        // if (!isDesc) setCurrentStep(index + 1);
+        // else setCurrentStep(index);
+        
+        if (!isDesc) setCurrentStep(index + 1);
+        else setCurrentStep(positions.length - index);
+    }
+
+    const sort = () => {
+        setPositions(prevPositions => {
+            const reversed = [...prevPositions].reverse();
+            console.log(prevPositions, reversed)
+            return reversed;
+        })
+        setIsDesc(prev => !prev);
     }
 
     const judgeWinner = (squares) => {
-        const lines = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-        ]
+        // const = ㄱ
+        
+        // // 변수 3개 , 가로 [] 세로 []대각선[]
+        // squares.map((square, index1) => {
+        //     // 가로 원소 1개    
+        //     square.map((item, index2) => {
+        //         // 세로 원소 1개
+        //         // 대각선
+        //         세로 변수.push(square[index1][index2])
+        //     })
+        //     // 한 행 순회 완료
+        //     // 한 행의 원소 3개가 모두 일치하는 가
+        // })
+
+        // // 새로 변수, 대각선 변수
+        // // 세로 변수 3개
+        // 세로 변수.map( => 원소 3개가 서로 일치하는 지 확인)
+
         lines.map(line => {
         const [a, b, c] = line;
-        if (history[currentStep][a] && history[currentStep][a] === history[currentStep][b] && history[currentStep][a] === history[currentStep][c]) {
-            setIsWin(history[currentStep][a]);
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            setIsWin(squares[a]);
         }
         })
         return;
@@ -65,12 +99,24 @@ const Board = () => {
                 </SquareSection>
             </div>
             <HistoryBlock>
-                {history.map((item, index) => {
-                    if (index === 0) {
-                        return <button key={index} onClick={() => move(index)}>시작 버튼</button>
-                    } else {
-                        return <button key={index} onClick={() => move(index)}>{index}번 버튼</button>
-                    }
+                <button onClick={sort}>정렬하기</button>
+                {/* <button onClick={() => move(index)}>시작 버튼</button> */}
+                
+                {/* {positions.map((position, index) => {
+                    return <button key={index} onClick={() => move(index)}>
+                        {currentStep - 1 === (isDesc ? currentStep - index - 1 : index )? 
+                            `당신은 # ${position.row} , ${position.col}에 있습니다.` : `${position.row} , ${position.col} 이동`}</button>
+                })} */}
+
+                {positions.map((position, index) => {
+                    return (
+                        <button key={index} onClick={() => move(index)}>
+                            {!isDesc ?
+                                (currentStep - 1 === index ? `당신은 # ${position.row} , ${position.col}에 있습니다.` : `${position.row} , ${position.col} 이동`) :
+                                (currentStep === positions.length - index ? `당신은 # ${position.row} , ${position.col}에 있습니다.` : `${position.row} , ${position.col} 이동`)
+                            }
+                        </button>
+                    )
                 })}
             </HistoryBlock>
         </Block>
