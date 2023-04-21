701ad491653727634842032cd5da220c531f3c3b
yunjink <chloeeekim1109@gmail.com>
2023-04-21 16:50:04 +0900

    3 X 3 Board 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
-function App() {
-  return <div className='App'>Tic-Tac-Toe</div>;
+function Square({ value }) {
+  function hanleClick() {
+    console.log('clicked');
+  }
+
+  return (
+    <button className='square' onClick={hanleClick}>
+      {value}
+    </button>
+  );
 }
 
-export default App;
+export default function Board() {
+  return (
+    <>
+      <div className='board-row'>
+        <Square value='1' />
+        <Square value='2' />
+        <Square value='3' />
+      </div>
+      <div className='board-row'>
+        <Square value='4' />
+        <Square value='5' />
+        <Square value='6' />
+      </div>
+      <div className='board-row'>
+        <Square value='7' />
+        <Square value='8' />
+        <Square value='9' />
+      </div>
+    </>
+  );
+}
